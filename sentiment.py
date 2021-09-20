57fc064d5b6912a22bf2391ff01c18b5ced2a71c
pedromps <90725927+pedromps@users.noreply.github.com>
2021-09-20 14:45:03 +0100

    Update sentiment.py
    added code to split

--- sentiment.py
+++ sentiment.py
@@ -2,12 +2,12 @@
 import os
 import numpy as np
 import pandas as pd
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from keras import preprocessing
 from keras.models import Sequential
 from keras.layers import LSTM, Embedding, Dense
 from keras.preprocessing.text import Tokenizer
-
+from sklearn.model_selection import train_test_split
 
 
 data_csv = pd.read_csv(os.path.join(os.getcwd(), "rotten_tomatoes_reviews.csv"), skipinitialspace=True)
@@ -15,21 +15,23 @@ data_csv = pd.read_csv(os.path.join(os.getcwd(), "rotten_tomatoes_reviews.csv"),
 reviews = data_csv["Review"]
 target = data_csv["Freshness"]
 
-# preprocessing
-
+# preprocessing, take from F. Chollet's Deep Learning with Python
 data = [reviews[i] for i in range(len(reviews))]
 data = np.array(data)
-
-texts = data
+# max length of each review
 maxlen = 100
-training_samples = 200
-validation_samples = 10000
+# max words for the tokenizer (and for the embedding layer later)
 max_words = 30000
 tokenizer = Tokenizer(num_words=max_words)
-tokenizer.fit_on_texts(texts)
-sequences = tokenizer.texts_to_sequences(texts)
+tokenizer.fit_on_texts(data)
+# tokenised sequences
+sequences = tokenizer.texts_to_sequences(data)
 
+# padding
 seq_train = preprocessing.sequence.pad_sequences(sequences, maxlen=maxlen)
+
+# train and test datasets
+x_train, x_test, y_train, y_test = train_test_split(seq_train, target)
 
 model = Sequential()
 model.add(Embedding(max_words, 32))
@@ -37,4 +39,7 @@ model.add(LSTM(32))
 model.add(Dense(1, activation='sigmoid'))
 model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
 model.summary()
-history = model.fit(seq_train, target, epochs=10, batch_size=128, validation_split=0.2)
+history = model.fit(x_train, y_train, epochs=10, batch_size=128, validation_split=0.1)
+
+# todo add graphing code
+# todo add testing 
