9c0fa8fec94445825f3e094f27b8835c5917ff56
pedromps <90725927+pedromps@users.noreply.github.com>
2021-09-20 14:26:00 +0100

    initial commit with the sentiment model
    added preprocessing of text

---
+++ sentiment.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+import os
+import numpy as np
+import pandas as pd
+# import matplotlib.pyplot as plt
+from keras import preprocessing
+from keras.models import Sequential
+from keras.layers import LSTM, Embedding, Dense
+from keras.preprocessing.text import Tokenizer
+
+
+
+data_csv = pd.read_csv(os.path.join(os.getcwd(), "rotten_tomatoes_reviews.csv"), skipinitialspace=True)
+
+reviews = data_csv["Review"]
+target = data_csv["Freshness"]
+
+# preprocessing
+
+data = [reviews[i] for i in range(len(reviews))]
+data = np.array(data)
+
+texts = data
+maxlen = 100
+training_samples = 200
+validation_samples = 10000
+max_words = 30000
+tokenizer = Tokenizer(num_words=max_words)
+tokenizer.fit_on_texts(texts)
+sequences = tokenizer.texts_to_sequences(texts)
+
+seq_train = preprocessing.sequence.pad_sequences(sequences, maxlen=maxlen)
+
+model = Sequential()
+model.add(Embedding(max_words, 32))
+model.add(LSTM(32))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
+model.summary()
+history = model.fit(seq_train, target, epochs=10, batch_size=128)#, validation_split=0.2)
+
+
+#-------------------------------------------------------------------------------
+
+from keras.datasets import imdb
+from keras import preprocessing
+
+max_features = 3*30000
+maxlen = 20
+(x_train, y_train), (x_test, y_test) = imdb.load_data()
+x_train = preprocessing.sequence.pad_sequences(x_train, maxlen=maxlen)
+x_test = preprocessing.sequence.pad_sequences(x_test, maxlen=maxlen)
+
+
+model = Sequential()
+model.add(Embedding(max_features, 32))
+model.add(LSTM(32))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
+model.summary()
+history = model.fit(x_train, y_train, epochs=10, batch_size=128, validation_split=0.2)
+
+
